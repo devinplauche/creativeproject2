d8a6f77c183ad7daa7203d79325bbc6e8bffad49
Devin Plauche <devinplauche@gmail.com>
2020-02-24 21:14:48 -0700

    2nd commit

--- stats.js
+++ stats.js
@@ -32,14 +32,21 @@ document.getElementById('playersubmit').addEventListener("click", function(event
           .then(function(response) {
             return response.json();
           }).then(function(json1) {
+
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
             var cell4 = row.insertCell(3);
-            var cell5 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
+            var cell6 = row.insertCell(5);
+            var cell7 = row.insertCell(6);
+
             cell2.innerHTML = json1.data[0].pts;
             cell3.innerHTML = json1.data[0].reb;
-            cell4.innerHTML = json1.data[0].fg_pct;
-            cell5.innerHTML = json1.data[0].ast;
+            cell4.innerHTML = json1.data[0].ast;
+            cell5.innerHTML = json1.data[0].fg_pct;
+            cell6.innerHTML = json1.data[0].fg3_pct;
+            cell7.innerHTML = json1.data[0].ft_pct;
+
 
             //document.getElementById("td2").innerHTML = json1.data[0].pts;
             //document.getElementById("td3").innerHTML = json1.data[0].reb;
